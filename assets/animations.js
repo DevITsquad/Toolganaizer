438ba9193b38ca4194c9dd4faff8debaea4cadd9
DevITsquad <dev.itsquad.agency@gmail.com>
2023-08-25 15:57:47 +0300

    updates

--- assets/animations.js
+++ assets/animations.js
@@ -2,6 +2,24 @@ const SCROLL_ANIMATION_TRIGGER_CLASSNAME = 'scroll-trigger';
 const SCROLL_ANIMATION_OFFSCREEN_CLASSNAME = 'scroll-trigger--offscreen';
 const SCROLL_ANIMATION_CANCEL_CLASSNAME = 'scroll-trigger--cancel';
 
+document.addEventListener("DOMContentLoaded", () => {
+  const emailBtn = document.getElementById('emailAsk');
+  emailBtn.addEventListener('click', scrollToElement)
+})
+function scrollToElement(e) {
+  e.preventDefault()
+  console.log(e.currentTarget)
+  const element = document.getElementById('leaveEmail');
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth', // You can also use 'auto' or 'instant'
+      block: 'end',     // Scroll to the top of the element
+      inline: 'nearest'   // Scroll to the nearest edge of the element horizontally
+    });
+  }
+}
+
+
 function onIntersection(elements, observer) {
   elements.forEach((element, index) => {
     if (element.isIntersecting) {

6bbc2d2da4a2ef849457e7f912413a884a060960
DevITsquad <dev.itsquad.agency@gmail.com>
2023-09-08 15:05:44 +0300

    updates

--- assets/reviewsCustomizationLogic.js
+++ assets/reviewsCustomizationLogic.js
@@ -4,6 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let wasCalledCounter = 0
     const maxCalls = 5
 
+    const custom_header = `
+    <h2 class="collection_custom markered_text"><p>real people ,<strong>real voices</strong></p></h2>
+    <div class="custom_reviews_text">
+    <span>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 0L11.1137 6.89121L18 6.87539L12.42 11.1186L14.5623 18L9 13.7312L3.43769 18L5.58 11.1186L0 6.87539L6.88632 6.89121L9 0Z" fill="#FBBC04"/>
+      </svg>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 0L11.1137 6.89121L18 6.87539L12.42 11.1186L14.5623 18L9 13.7312L3.43769 18L5.58 11.1186L0 6.87539L6.88632 6.89121L9 0Z" fill="#FBBC04"/>
+      </svg>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 0L11.1137 6.89121L18 6.87539L12.42 11.1186L14.5623 18L9 13.7312L3.43769 18L5.58 11.1186L0 6.87539L6.88632 6.89121L9 0Z" fill="#FBBC04"/>
+      </svg>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 0L11.1137 6.89121L18 6.87539L12.42 11.1186L14.5623 18L9 13.7312L3.43769 18L5.58 11.1186L0 6.87539L6.88632 6.89121L9 0Z" fill="#FBBC04"/>
+      </svg>
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9 0L11.1137 6.89121L18 6.87539L12.42 11.1186L14.5623 18L9 13.7312L3.43769 18L5.58 11.1186L0 6.87539L6.88632 6.89121L9 0Z" fill="#FBBC04"/>
+      </svg>
+     </span>
+     <p><strong>4.8/5</strong>Based On<strong>700+</strong>Happy Customers Reviews</p>
+    </div>
+     `
+
     function handleWhSliderMutation(mutationsList) {
         for (const mutation of mutationsList) {
             if (mutation.type === 'childList') {
@@ -14,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         mainReviewsWrapper.style.minHeight = '610px';
                         mainReviewsWrapper.style.border = '1px solid rgb(255, 253, 241, 1)';
 
-                        const intervalId = setInterval(() => innerCustomObserver(intervalId), 1500)
+                        const intervalId = setInterval(() => innerCustomObserver(intervalId ,custom_header), 1500)
 
 
 
@@ -28,12 +52,13 @@ document.addEventListener('DOMContentLoaded', () => {
     whSliderObserver.observe(document, { childList: true, subtree: true, attributes: true });
 
 
-    function innerCustomObserver(intervalId) {
+    function innerCustomObserver(intervalId , custom_header) {
         wasCalledCounter++
         const allData = document.querySelectorAll('.alr-wh-review-com-is-isLineClamp')
         const verifiedSocket = document.querySelectorAll('.alr-wh-account-info-row')
-
-
+        const header = document.querySelector('.alr-review-summary-title')
+        header.innerHTML = '';
+        header.insertAdjacentHTML('afterend', custom_header);
         if (allData.length) {
 
             verifiedSocket.forEach(el => {

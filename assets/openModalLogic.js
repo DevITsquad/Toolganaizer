72da74d2180799adbdee56f14f56571b2e83307f
kolac300 <kolac300@gmail.com>
2024-01-10 13:41:25 +0200

    update

--- assets/openModalLogic.js
+++ assets/openModalLogic.js
@@ -1,58 +1,72 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     // Get references to the modal and close button
-    const modal = document.getElementById('popup_Modal');
-    const closeButton = document.getElementById('closePopup');
+    const modal = document.querySelector(".modal:not(.swiper-modal)");
+    const swiperModal = document.querySelector(".swiper-modal");
+    const closeButton = document.querySelectorAll(".close");
 
     // Function to show the modal
     function openModal(e) {
-        const popupURL = e.currentTarget.getAttribute('src');
-        document.querySelector('#popup_Modal .popup_img').setAttribute('src', popupURL);
-        modal.style.display = 'block';
-        document.body.style.overflow = 'hidden'; // Disable scrolling
+        const popupURL = e.currentTarget.getAttribute("src");
+
+        document
+            .querySelector("#popup_Modal .popup_img")
+            .setAttribute("src", popupURL);
+
+        if (e.currentTarget.alt === "product_image") {
+            modal.style.display = "none";
+            swiperModal.style.display = "block";
+        } else {
+            modal.style.display = "block";
+            //document.body.style.overflow = "hidden";
+        }
+        document.body.style.overflow = "hidden"; // Disable scrolling
 
         // Add event listener to close when clicking outside the modal
-        modal.addEventListener('click', closeModalOnClickOutside);
+        swiperModal.addEventListener("click", closeModalOnClickOutside);
+        modal.addEventListener("click", closeModalOnClickOutside);
 
         // Add event listener to close when pressing the Escape key
-        document.addEventListener('keydown', closeModalOnEscape);
+        document.addEventListener("keydown", closeModalOnEscape);
     }
 
     // Function to hide the modal
-    function closeModal() {
-        modal.style.display = 'none';
-        document.body.style.overflow = 'auto'; // Enable scrolling
+    function closeModal(e) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto"; // Enable scrolling
+        swiperModal.style.display = "none";
 
         // Remove event listeners for clicking outside and pressing Escape
-        modal.removeEventListener('click', closeModalOnClickOutside);
-        document.removeEventListener('keydown', closeModalOnEscape);
+        swiperModal.removeEventListener("click", closeModalOnClickOutside);
+        modal.removeEventListener("click", closeModalOnClickOutside);
+        document.removeEventListener("keydown", closeModalOnEscape);
     }
 
     // Function to close when clicking outside the modal
     function closeModalOnClickOutside(e) {
-        if (e.target === modal) {
+        if (e.target === modal || e.target === swiperModal) {
             closeModal();
         }
     }
 
     // Function to close when pressing the Escape key
     function closeModalOnEscape(e) {
-        if (e.key === 'Escape') {
+        if (e.key === "Escape") {
             closeModal();
         }
     }
 
     // Attach event listeners to open and close the modal
-    document.querySelectorAll('.bottom_images_slider_wrapper .swiper-slide img').forEach((el) => {
-        el.addEventListener('click', openModal);
+    document
+        .querySelectorAll(".bottom_images_slider_wrapper .swiper-slide img")
+        .forEach((el) => {
+            el.addEventListener("click", openModal);
+        });
+    document
+        .querySelectorAll(".main-product-custom_slider_wrapper .swiper-main-img")
+        .forEach((el) => {
+            el.addEventListener("click", openModal);
+        });
 
-        
-    });
-    document.querySelectorAll('.main-product-custom_slider_wrapper .swiper-main-img').forEach((el) => {
-        el.addEventListener('click', openModal);
-
-
-    });
-    closeButton.addEventListener('click', closeModal);
-
-})
+    //   closeButton.addEventListener("click", closeModal);
+    closeButton.forEach((el) => el.addEventListener("click", closeModal));
+});
